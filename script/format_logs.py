2a8e06f47f183c9f786676bc8e2573232b0d6403
kazumakano <kazumakano@outlook.jp>
2022-03-19 20:33:31 +0900

    apply rotation vector

--- script/format_logs.py
+++ script/format_logs.py
@@ -16,31 +16,34 @@ def _set_params(conf_file: Union[str, None] = None) -> None:
     ROOT_DIR = path.join(path.dirname(__file__), "../")
 
     if conf_file is None:
-        conf_file = path.join(ROOT_DIR, "config/default.yaml")    # load default config file
+        conf_file = path.join(ROOT_DIR, "config/default.yaml")
     print(f"{path.basename(conf_file)} has been loaded")
 
     with open(conf_file) as f:
         conf: dict[str, Any] = yaml.safe_load(f)
     ENABLE_BLE = bool(conf["enable_ble"])
-    FREQ = np.float16(conf["freq"])
+    FREQ = np.float32(conf["freq"])
     INERTIAL_SENSORS = tuple(conf["inertial_sensors"])
 
 def _load_log(file: str) -> tuple[np.ndarray, np.ndarray]:
     inertial = np.empty(len(INERTIAL_SENSORS), dtype=np.ndarray)
-    for i in range(len(INERTIAL_SENSORS)):
-        inertial[i] = np.empty((0, 4), dtype=np.float64)    # (timestamp, x, y, z)
+    for i, s in enumerate(INERTIAL_SENSORS):
+        if s in ("ACC", "GRAV", "GYRO"):
+            inertial[i] = np.empty((0, 4), dtype=np.float64)    # (timestamp, x, y, z)
+        elif s == "ROTV":
+            inertial[i] = np.empty((0, 5), dtype=np.float64)    # (timestamp, x, y, z, scalar)
     ble = np.empty(3, dtype=np.ndarray)
     ble[0] = np.empty(0, dtype=np.float64)    # timestamp
     ble[1] = np.empty(0, dtype=str)           # MAC address
     ble[2] = np.empty(0, dtype=np.int8)       # RSSI (>= -128)
 
     with open(file) as f:
-        reader = csv.reader(f, delimiter="\t")
-        for row in reader:
+        for row in csv.reader(f, delimiter="\t"):
             for i, s in enumerate(INERTIAL_SENSORS):
                 if s == row[1]:
                     row_2 = row[2].split(",")[1:]
-                    inertial[i] = np.vstack((inertial[i], (np.float64(row[0]), np.float64(row_2[0]), np.float64(row_2[1]), np.float64(row_2[2]))))
+                    inertial[i] = np.vstack((inertial[i], (np.float64(row[0]), *[np.float64(v) for v in row_2])))
+                    break
 
             if ENABLE_BLE and row[1] == "BLE":
                 row_2 = row[2].split(",")
@@ -54,11 +57,17 @@ def _load_log(file: str) -> tuple[np.ndarray, np.ndarray]:
 
 def _resample_inertial_log(data: np.ndarray) -> tuple[np.ndarray, np.ndarray]:
     resampled_ts = np.arange(max(d[0, 0] for d in data), min(d[-1, 0] for d in data), step=1/FREQ, dtype=np.float64)
+    resampled_val = np.empty((len(resampled_ts), 3 * len(INERTIAL_SENSORS) + 1 if "ROTV" in INERTIAL_SENSORS else 3 * len(INERTIAL_SENSORS)), dtype=np.float64)
 
-    resampled_val = np.empty((len(resampled_ts), 3 * len(INERTIAL_SENSORS)), np.float64)
-    for i, d in enumerate(data):
-        for j in range(3):
-            resampled_val[:, 3*i+j] = interp1d(d[:, 0], d[:, j+1])(resampled_ts)
+    col_index = 0
+    for sensor_index, s in enumerate(INERTIAL_SENSORS):
+        if s in ("ACC", "GRAV", "GYRO"):
+            val_len = 3
+        elif s == "ROTV":
+            val_len = 4
+        for i in range(val_len):
+            resampled_val[:, col_index + i] = interp1d(data[sensor_index][:, 0], data[sensor_index][:, i + 1])(resampled_ts)
+        col_index += val_len
 
     return resampled_ts, resampled_val
 
