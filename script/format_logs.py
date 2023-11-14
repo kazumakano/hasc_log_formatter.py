84ad0b173966f9d3386bc0d985c1277a24faa87d
kazumakano <kazumakano@outlook.jp>
2023-11-14 20:10:41 +0900

    apply wifi and gaming rotation vector

--- script/format_logs.py
+++ script/format_logs.py
@@ -12,7 +12,7 @@ from scipy.interpolate import interp1d
 
 
 def _set_params(conf_file: str | None) -> None:
-    global ROOT_DIR, INERTIAL_SENSORS, ENABLE_BLE, FREQ
+    global ROOT_DIR, INERTIAL_SENSORS, ENABLE_BLE, ENABLE_WIFI, FREQ
 
     ROOT_DIR = path.join(path.dirname(__file__), "../")
 
@@ -23,20 +23,25 @@ def _set_params(conf_file: str | None) -> None:
     with open(conf_file) as f:
         conf: dict[str, Any] = yaml.safe_load(f)
     ENABLE_BLE = bool(conf["enable_ble"])
+    ENABLE_WIFI = bool(conf["enable_wifi"])
     FREQ = np.float32(conf["freq"])
     INERTIAL_SENSORS = tuple(conf["inertial_sensors"])
 
-def _load_log(file: str) -> tuple[np.ndarray, np.ndarray]:
+def _load_log(file: str) -> tuple[np.ndarray, np.ndarray, np.ndarray]:
     inertial = np.empty(len(INERTIAL_SENSORS), dtype=np.ndarray)
     for i, s in enumerate(INERTIAL_SENSORS):
         if s in ("ACC", "GRAV", "GYRO", "MAG"):
             inertial[i] = np.empty((0, 4), dtype=np.float64)    # (timestamp, x, y, z)
-        elif s == "ROTV":
+        elif s in ("ROTV", "GROTV"):
             inertial[i] = np.empty((0, 5), dtype=np.float64)    # (timestamp, x, y, z, scalar)
     ble = np.empty(3, dtype=np.ndarray)
     ble[0] = np.empty(0, dtype=np.float64)    # timestamp
     ble[1] = np.empty(0, dtype=str)           # MAC address
     ble[2] = np.empty(0, dtype=np.int8)       # RSSI (>= -128)
+    wifi = np.empty(3, dtype=np.ndarray)
+    wifi[0] = np.empty(0, dtype=np.float64)
+    wifi[1] = np.empty(0, dtype=str)
+    wifi[2] = np.empty(0, dtype=np.int8)
 
     print(f"loading {path.basename(file)}")
 
@@ -53,19 +58,30 @@ def _load_log(file: str) -> tuple[np.ndarray, np.ndarray]:
                 ble[1] = np.hstack((ble[1], row_2[0].lower()))
                 ble[2] = np.hstack((ble[2], np.int8(row_2[1])))
 
+            if ENABLE_WIFI and row[1] == "WIFI" and len(row[2]) > 0:
+                row_2 = row[2].split(",")
+                for c in row_2:
+                    c = c.split("|")
+                    if len(c) == 3:
+                        wifi[0] = np.hstack((wifi[0], np.float64(row[0])))
+                        wifi[1] = np.hstack((wifi[1], c[0].lower()))
+                        wifi[2] = np.hstack((wifi[2], np.int8(c[2])))
+                    else:
+                        print(f"failed to parse wifi data at {row[0]}")
+
     print(f"{path.basename(file)} has been loaded")
 
-    return inertial, ble
+    return inertial, ble, wifi
 
 def _resample_inertial_log(data: np.ndarray) -> tuple[np.ndarray, np.ndarray]:
     resampled_ts = np.arange(max(d[0, 0] for d in data), min(d[-1, 0] for d in data), step=1/FREQ, dtype=np.float64)
-    resampled_val = np.empty((len(resampled_ts), 3 * len(INERTIAL_SENSORS) + 1 if "ROTV" in INERTIAL_SENSORS else 3 * len(INERTIAL_SENSORS)), dtype=np.float64)
+    resampled_val = np.empty((len(resampled_ts), 3 * len(INERTIAL_SENSORS) + INERTIAL_SENSORS.count("ROTV") + INERTIAL_SENSORS.count("GROTV")), dtype=np.float64)
 
     col_index = 0
     for sensor_index, s in enumerate(INERTIAL_SENSORS):
         if s in ("ACC", "GRAV", "GYRO", "MAG"):
             val_len = 3
-        elif s == "ROTV":
+        elif s in ("ROTV", "GROTV"):
             val_len = 4
         for i in range(val_len):
             resampled_val[:, col_index + i] = interp1d(data[sensor_index][:, 0], data[sensor_index][:, i + 1])(resampled_ts)
@@ -82,8 +98,8 @@ def _unix2datetime(ts: np.ndarray) -> np.ndarray:
     return ts.astype(datetime)
 
 def _format_log(src_file: str, tgt_dir: str, label: Optional[str]) -> None:
-    inertial, ble = _load_log(src_file)
-    
+    inertial, ble, wifi = _load_log(src_file)
+
     resampled_ts, resampled_val = _resample_inertial_log(inertial)
     resampled_ts = _unix2datetime(resampled_ts)
 
@@ -91,37 +107,66 @@ def _format_log(src_file: str, tgt_dir: str, label: Optional[str]) -> None:
     dir = path.join(tgt_dir, "inertial/")
     if not path.exists(dir):
         mkdir(dir)
-    inertial_tgt_file_name = path.splitext(path.basename(src_file))[0] + "_inertial_" + "".join(s[0].lower() for s in INERTIAL_SENSORS) + ("" if label is None else "_" + label)
-    tgt_file = path.join(dir, inertial_tgt_file_name + ".csv")
+    tgt_file_name = path.splitext(path.basename(src_file))[0] + "_inertial_" + "".join(s[0].lower() for s in INERTIAL_SENSORS) + ("" if label is None else "_" + label)
+    tgt_file = path.join(dir, tgt_file_name + ".csv")
     with open(tgt_file, mode="w", newline="") as f:
         writer = csv.writer(f)
         t: datetime
         for i, t in enumerate(resampled_ts):
             writer.writerow((t.strftime("%Y-%m-%d %H:%M:%S.%f"), *resampled_val[i]))
 
-    print(f"written to inertial/{path.basename(tgt_file)}")
+    print(f"written to inertial/{tgt_file_name}.csv")
 
-    tgt_file = path.join(dir, inertial_tgt_file_name + ".pkl")
+    tgt_file = path.join(dir, tgt_file_name + ".pkl")
     with open(tgt_file, mode="wb") as f:
         pickle.dump((resampled_ts, resampled_val), f)
     
-    print(f"written to inertial/{path.basename(tgt_file)}")
+    print(f"written to inertial/{tgt_file_name}.pkl")
 
     # ble
     if ENABLE_BLE:
         ble[0] = _unix2datetime(ble[0])
 
-        dir = path.join(tgt_dir, "ble/")
+        dir = path.join(tgt_dir, "radio/")
         if not path.exists(dir):
             mkdir(dir)
-        tgt_file = path.join(dir, path.splitext(path.basename(src_file))[0] + "_ble" + ("" if label is None else "_" + label) + ".csv")
+        tgt_file_name = path.splitext(path.basename(src_file))[0] + "_ble" + ("" if label is None else "_" + label)
+        tgt_file = path.join(dir, tgt_file_name + ".csv")
         with open(tgt_file, mode="w", newline="") as f:
             writer = csv.writer(f)
             t: datetime
             for i, t in enumerate(ble[0]):
                 writer.writerow((t.strftime("%Y-%m-%d %H:%M:%S.%f"), ble[1][i], ble[2][i]))
 
-        print(f"written to ble/{path.basename(tgt_file)}")
+        print(f"written to radio/{tgt_file_name}.csv")
+
+        tgt_file = path.join(dir, tgt_file_name + ".pkl")
+        with open(tgt_file, mode="wb") as f:
+            pickle.dump(ble, f)
+
+        print(f"written to radio/{tgt_file_name}.pkl")
+
+    if ENABLE_WIFI:
+        wifi[0] = _unix2datetime(wifi[0])
+
+        dir = path.join(tgt_dir, "radio/")
+        if not path.exists(dir):
+            mkdir(dir)
+        tgt_file_name = path.splitext(path.basename(src_file))[0] + "_wifi" + ("" if label is None else "_" + label)
+        tgt_file = path.join(dir, tgt_file_name + ".csv")
+        with open(tgt_file, mode="w", newline="") as f:
+            writer = csv.writer(f)
+            t: datetime
+            for i, t in enumerate(wifi[0]):
+                writer.writerow((t.strftime("%Y-%m-%d %H:%M:%S.%f"), wifi[1][i], wifi[2][i]))
+
+        print(f"written to radio/{tgt_file_name}.csv")
+
+        tgt_file = path.join(dir, tgt_file_name + ".pkl")
+        with open(tgt_file, mode="wb") as f:
+            pickle.dump(wifi, f)
+
+        print(f"written to radio/{tgt_file_name}.pkl")
 
 def format_logs(src_file: Optional[str] = None, src_dir: Optional[str] = None, tgt_dir: Optional[str] = None, label: Optional[str] = None) -> None:
     if tgt_dir is None:
